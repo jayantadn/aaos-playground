da9b4f124e689acb7ef68f09ce4d82f62ad30eb1
Jayanta <Jayanta.dn@gmail.com>
2026-04-10 05:54:33 +0000

    Refactor build script to remove conditional repo initialization and sync

--- build.sh
+++ build.sh
@@ -1,18 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 ANDROID_BRANCH="android12L-release"
 REPO_URL="https://android.googlesource.com/platform/manifest"
 
-if [ ! -d "$ANDROID_BRANCH/.repo" ]; then
-	mkdir -p $ANDROID_BRANCH
-	cd $ANDROID_BRANCH
-	repo init -u $REPO_URL -b $ANDROID_BRANCH
-	repo sync -j8
-else
-	cd $ANDROID_BRANCH
-	echo "Android repository already exists. Skipping repo init and sync."
-fi
+mkdir -p $ANDROID_BRANCH 
+cd $ANDROID_BRANCH 
+repo init -u $REPO_URL -b $ANDROID_BRANCH
+repo sync -j8
 
-. build/envsetup.sh && lunch sdk_car_x86_64-userdebug && m -j24
+. build/envsetup.sh && lunch sdk_car_x86_64-userdebug && m -j$(nproc)
 
 
